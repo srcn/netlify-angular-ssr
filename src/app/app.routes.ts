ae136aa21c12e52262605096b6b91f9d95e5c6f2
Sercan Yemen <117369+srcn@users.noreply.github.com>
2024-05-27 12:24:21 +0300

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import HelloComponent from './hello/hello.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: 'hello', component: HelloComponent }];
---
+++ src/app/hello/hello.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hello',
+  standalone: true,
+  template: `<h1>Hello World</h1>`,
+})
+export default class HelloComponent {}
