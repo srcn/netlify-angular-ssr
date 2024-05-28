b632cd511b1d048c85a4f53d36a51d0ecd1966d1
Sercan Yemen <117369+srcn@users.noreply.github.com>
2024-05-28 13:07:16 +0300

    ssr request and context test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,16 @@
-import { Component, Inject, Optional } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Context } from '@netlify/functions';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector  : 'app-root',
+    standalone: true,
+    imports   : [
+        RouterOutlet,
+    ],
+    template  : `
+        <router-outlet/>
+    `,
 })
 export class AppComponent {
   title = 'netlify-ssr-functions-test';
-
-    constructor(
-        @Inject('netlify.request') @Optional() request?: Request,
-        @Inject('netlify.context') @Optional() context?: Context
-    )
-    {
-        console.log('Request:', request?.url);
-        console.log('Context:', context?.geo.city);
-    }
-
 }
--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Inject, Optional } from '@angular/core';
+import { Context } from '@netlify/functions';
 
 @Component({
   selector: 'app-hello',
   standalone: true,
-  template: `<h1>Hello World</h1>`,
+  host: {ngSkipHydration: 'true'},
+  template: `
+      <h1>Hello World</h1>
+      <div>City:
+          <b>{{ context?.geo?.city ?? 'undefined' }}</b>
+      </div>
+  `,
 })
-export default class HelloComponent {}
+export default class HelloComponent {
+
+    constructor(
+        @Inject('netlify.request') @Optional() public request?: Request,
+        @Inject('netlify.context') @Optional() public context?: Context
+    )
+    {
+        console.log('Request:', this.request?.url);
+        console.log('Context:', this.context?.geo.city);
+    }
+}
